1d2a5b4b14c34ff9dfe28354a4fb0123c2516f5c
Sameer Dhar <avielus@gmail.com>
2014-02-12 17:13:41 -0500

    Working on routing

--- app/models/task.js
+++ app/models/task.js
@@ -7,7 +7,25 @@ var Task = DS.Model.extend({
     defaultValue: function () { return new Date(); }
   }), 
 
-  user: DS.belongsTo('user')
+  //user: DS.belongsTo('user')
 }); 
+
+
+Task.reopenClass({
+  FIXTURES: [
+  {
+    id: "1",
+    title: 'install EAK',
+    isCompleted: true
+  }, {
+    id: "2",
+    title: 'install additional dependencies',
+    isCompleted: true
+  }, {
+    id: "3",
+    title: 'develop amazing things',
+    isCompleted: false
+  }
+]});
 
 export default Task; 
